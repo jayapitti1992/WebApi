9bc8fdc3d87f23d6f78cb8755d0fdfe7b8afa71b
jaypitti <jaypitti@WIN-62GDBT41VHF>
2017-08-16 14:11:44 -0500

    Add project files.

---
+++ WebTask/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebTask.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTask.WebApiApplication" Language="C#" %>
